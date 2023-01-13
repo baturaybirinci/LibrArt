ce05aba003ecc9a9f4d01d1701d0c9be303475c0
Işıktan <isiktantanis@gmail.com>
2023-01-13 22:03:59 +0300

    reformed backend

--- backend-master/librart_main/admin.py
+++ backend-master/librart_main/admin.py
@@ -1,25 +1,5 @@
 from django.contrib import admin
-from .models import ContentCreator, Collection, CollectionImage, NFT
+from .models import User, Collection
 
-
-class ContentCreatorModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'address', 'slug', 'created_at')
-    search_fields = ('name', 'address')
-    list_per_page = 25
-
-
-class CollectionModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'creator', 'created_at', 'updated_at')
-    search_fields = ('name', 'creator__name')
-    list_per_page = 25
-
-class CollectionImageModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'owned_by', 'created_at', 'updated_at')
-    search_fields = ('name', 'owned_by__name')
-    list_per_page = 25
-
-
-admin.site.register(ContentCreator, ContentCreatorModelAdmin)
-admin.site.register(CollectionImage, CollectionImageModelAdmin)
-admin.site.register(Collection, CollectionModelAdmin)
-admin.site.register(NFT)
+admin.site.register(User)
+admin.site.register(Collection)
--- backend-master/librart_main/models.py
+++ backend-master/librart_main/models.py
@@ -1,81 +1,34 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.template.defaultfilters import slugify
-from django.core.validators import MaxValueValidator, MinValueValidator
 
-def current_year():
-    return datetime.date.today().year
-
-def max_value_current_year(value):
-    return MaxValueValidator(current_year())(value)
-
-class ContentCreator(models.Model):
-    class CreatorType(models.TextChoices):
+class User(models.Model):
+    class UserType(models.TextChoices):
         MUSEUM = 'MU', _('Museum')
         INDIVIDUAL = 'IN', _('Individual')
 
     creator_type = models.CharField(
         max_length = 2,
-        choices = CreatorType.choices,
-        default = CreatorType.MUSEUM
+        choices = UserType.choices,
+        default = UserType.MUSEUM
     )
 
     name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add = True)
+    last_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, primary_key=True)
+    avatar = models.ImageField(null=True, blank=True)
 
     # add banner image
 
-    def get_creator_type(self) -> CreatorType:
+    def get_creator_type(self) -> UserType:
         # Get value from choices enum
-        return self.CreatorType[self.creator_type]
+        return self.UserType[self.creator_type]
 
-    def __str__(self):
-        return self.slug
-
-    def save(self, *args, **kwargs):
-        to_assign = slugify(self.name)
-
-        if ContentCreator.objects.filter(slug = to_assign).exists():
-            to_assign = f'{to_assign}-{ContentCreator.objects.all().count()}'
-
-        self.slug = to_assign
-        super().save(*args, **kwargs)
 
 
 class Collection(models.Model):
-    creator = models.ForeignKey(ContentCreator, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="collections")
     name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
-    def save(self, *args, **kwargs):
-        to_assign = slugify(self.name)
+    address = models.CharField(max_length=255, primary_key=True)
+    avatar = models.ImageField(null=True, blank=True)
+    banner = models.ImageField(null=True, blank=True)
 
-        if ContentCreator.objects.filter(slug = to_assign).exists():
-            to_assign = f'{to_assign}-{ContentCreator.objects.all().count()}'
-
-        self.slug = to_assign
-        super().save(*args, **kwargs)
-
-class CollectionImage(models.Model):
-    owned_by = models.ForeignKey(Collection, on_delete=models.CASCADE)
-    path = models.FilePathField(path="/images", match=None, recursive=False, max_length=255)
-    image_type = models.CharField(max_length=255)
-    year = models.IntegerField(_('year'), validators=[MinValueValidator(1984), max_value_current_year])
-    name = models.CharField(max_length=255)
-    artist = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
-    def save(self, *args, **kwargs):
-        to_assign = slugify(self.name)
-
-        if ContentCreator.objects.filter(slug = to_assign).exists():
-            to_assign = f'{to_assign}-{ContentCreator.objects.all().count()}'
-
-        self.slug = to_assign
-        super().save(*args, **kwargs)
-
-class NFT(models.Model):
-    name = models.CharField(max_length=255)
