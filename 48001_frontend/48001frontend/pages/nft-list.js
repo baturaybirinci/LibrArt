944946fbfe93be0ffd73b3bedf984d44e4c81a2a
baturaybirinci <57406116+baturaybirinci@users.noreply.github.com>
2023-01-15 05:23:30 +0300

    nft list v2

--- 48001_frontend/48001frontend/pages/nft-list.js
+++ 48001_frontend/48001frontend/pages/nft-list.js
@@ -3,39 +3,61 @@ import LibrartNavbar from "../components/librart-navbar";
 const abiPathNft = require("../public/TestNFT.json");
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
-import { times } from "lodash";
 
 export default function nftList() {
   const [list, setList] = useState([]);
   const [adr, setAdr] = useState([]);
   const Collections = require("../public/collections.json");
   let items = [];
-
+  let isIterated = false
   const router = useRouter();
-  
-  const web3 =  new Web3(window.ethereum);
-  const contract =  new web3.eth.Contract(abiPathNft["abi"], '0x5f50e2a874b23fd3e3666975fcde6be20e2a52fa');
 
-  const iter = async (i) => {
+  const web3 = new Web3(window.ethereum);
+  const contract = new web3.eth.Contract(
+    abiPathNft["abi"],
+    "0x5f50e2a874b23fd3e3666975fcde6be20e2a52fa"
+  );
 
-    const res = await contract.methods.tokenURI(i).call()
-    console.log(res)
-    return res;
-  };
-   useEffect(() => {
-    setAdr(router.query.address)
-    for(let i = 0; i < 5; i++){
-      iter(i).then((res) => setList([...list,res])
-      );
-    }
+  const iter = async () => {
+    if(!isIterated){
+    for (let i = 0; i < 5; i++) {
+      contract.methods
+        .tokenURI(i)
+        .call()
+        .then((res) => {
+          console.log(res)
+          setList((list) => [...list, res]);
+        });
+    }}
+    contract.methods.exists(5).call().then((exists) => {
+      if(exists){
+          console.log("NFT ID exists in the contract");
+      }else{
+          console.log("NFT ID does not exist in the contract");
+      }
   });
-  iter();
+  
+  
+  
+    isIterated = true
+  };
+  useEffect(() => {
+    setAdr(router.query.address);
+    iter();
+
+  }, []);
+
   return (
     <>
       <LibrartNavbar />
-      <div>{list.map((element) => (
-        <div>{element}</div>
-      ))}</div>
+      <div>
+        <div>
+          {list.length}
+        </div>
+        {list.map((element) => (
+          <div>{element}</div>
+        ))}
+      </div>
     </>
   );
 }
